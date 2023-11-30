ced9db5453ff3ec4bb015203ba5a6df2ed234d84
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-11-30 22:18:57 +0530

    model trainer

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
 from mice.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
@@ -23,6 +24,7 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
 from mice.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Transformation stage"
@@ -30,6 +32,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+from mice.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/mice/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,14 @@
+from mice.config import ConfigurationManager
+from mice.components.model_trainer import ModelTrainer
+
+
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
