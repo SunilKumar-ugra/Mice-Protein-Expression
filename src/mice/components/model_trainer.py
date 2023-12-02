9ff60b685a04c51c34f0f21260e1b881c9a4edba
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-12-02 23:49:21 +0530

    model_trainer

--- src/mice/components/model_trainer.py
+++ src/mice/components/model_trainer.py
@@ -4,6 +4,7 @@ import os
 from mice import logger
 from sklearn.linear_model import ElasticNet
 import joblib
+import xgboost as xgb
 
 class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig):
@@ -21,8 +22,10 @@ class ModelTrainer:
         test_y = test_data[[self.config.target_column]]
 
 
-        lr = ElasticNet(alpha=self.config.alpha, l1_ratio=self.config.l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
-
-        joblib.dump(lr, os.path.join(self.config.root_dir, self.config.model_name))
+        # lr = ElasticNet(alpha=self.config.alpha, l1_ratio=self.config.l1_ratio, random_state=42)
+        # lr.fit(train_x, train_y)
+        
+        xgboost=xgb.XGBClassifier(objective="multi:softprob", random_state=42)
+        xgboost.fit(train_x, train_y)
+        joblib.dump(xgboost, os.path.join(self.config.root_dir, self.config.model_name))
 
