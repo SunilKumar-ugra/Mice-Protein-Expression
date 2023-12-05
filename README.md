e3ec1bdc3cde09ea2ab27ccb1d9a2d1d590825d9
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-12-05 18:32:04 +0530

    asure deployment

--- README.md
+++ README.md
@@ -139,7 +139,7 @@ DVC
 
 	
 ## 3. Create ECR repo to store/save docker image
-    - Save the URI: 566373416292.dkr.ecr.us-east-1.amazonaws.com/chicken
+    - Save the URI: 396709481203.dkr.ecr.ap-south-1.amazonaws.com/mice
 
 	
 ## 4. Create EC2 machine (Ubuntu) 
--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def index():
 
 if __name__ == "__main__":
 	# app.run(host="0.0.0.0", port = 8080, debug=True)
-	app.run(host="0.0.0.0", port = 8080)
+	# app.run(host="0.0.0.0", port = 8080) #for AWS /Local host
+    app.run(host="0.0.0.0", port = 80) # Asure 
